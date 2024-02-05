abfddffa8e1fe576415a6d0f3ef7522c33bd96bd
SonyT0pran0 <119934629+ShaktiSankalp@users.noreply.github.com>
2024-02-05 14:42:24 +0530

    first commit

---
+++ deleteit.js
@@ -0,0 +1,16 @@
+let name ='thala'
+let no = 7
+let isLegend = true;
+console.log(typeof name);
+console.log(typeof no);
+console.log(typeof isLegend);
+console.log('5'+'5' - 5);
+
+let person = {
+    name: 'jhon',
+    age: 30,
+    occupation: 'Developer'
+}
+for(let key in person){
+    console.log(key+": " + person[key]);
+}
